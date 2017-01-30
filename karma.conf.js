31fffc2752d86a0428f7604c476a57e5fcfd249c
Pine Mizune <pinemz@gmail.com>
2017-01-31 01:10:40 +0900

    chore(travis): test in source labs

--- karma.conf.js
+++ karma.conf.js
@@ -1,10 +1,52 @@
 'use strict'
 
+const customLaunchers = {
+  sl_chrome: {
+    base: 'SauceLabs',
+    browserName: 'chrome',
+    platform: 'Windows 10',
+    version: '51.0'
+  },
+  sl_firefox: {
+    base: 'SauceLabs',
+    browserName: 'firefox',
+    platform: 'Windows 10',
+    version: '47.0'
+  },
+  sl_ie: {
+    base: 'SauceLabs',
+    browserName: 'internet explorer',
+    platform: 'Windows 7',
+    version: '11.0',
+  },
+  sl_edge: {
+    base: 'SauceLabs',
+    browserName: 'MicrosoftEdge',
+    platform: 'Windows 10',
+    version: '13.10586'
+  },
+  sl_safari: {
+    base: 'SauceLabs',
+    browserName: 'safari',
+    platform: 'OS X 10.11',
+    version: '9.0',
+  },
+}
+
+const isTravis   = process.env.TRAVIS === 'true'
+const isFirstJob = /\.1$/.test(process.env.TRAVIS_JOB_NUMBER)
+
+const browsers = ['PhantomJS']
+if (isTravis && isFirstJob) {
+  Array.prototype.push.apply(browsers, Object.keys(customLaunchers))
+}
+
 module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['mocha'],
-    browsers: ['PhantomJS'],
+    browsers: browsers,
+    customLaunchers: customLaunchers,
 
     files: [
       'test/**/*_test.ts',
